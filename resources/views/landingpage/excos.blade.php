2b69d076a8da4e403e95d58ae251a8abfd0a558a
lawalthb <38714153+lawalthb@users.noreply.github.com>
2024-04-07 19:28:01 +0100

    excos done

--- resources/views/landingpage/excos.blade.php
+++ resources/views/landingpage/excos.blade.php
@@ -1,85 +1,43 @@
+@php
+$excos = App\Models\WebExcos::orderBy('position')->get();
+
+
+@endphp
+
 <!-- ======= Excos Section ======= -->
 <section id="excos" class="doctors section-bg">
   <div class="container" data-aos="fade-up">
 
     <div class="section-title">
       <h2>Excos</h2>
-      <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-    </div>
+         </div>
 
     <div class="row">
+    @foreach ( $excos as $key=> $exco )
+  
+          <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+            <div class="member" data-aos="fade-up" data-aos-delay="{{$key+1}}00">
+              <div class="member-img">
+                <img src="{{asset($exco->image)}}" class="img-fluid" alt="">
+                <!-- <div class="social">
+                  <a href=""><i class="bi bi-twitter"></i></a>
+                  <a href=""><i class="bi bi-facebook"></i></a>
+                  <a href=""><i class="bi bi-instagram"></i></a>
+                  <a href=""><i class="bi bi-linkedin"></i></a>
+                </div> -->
+              </div>
+              <div class="member-info">
+                <h4>{{$exco->name}}</h4>
+                <span>{{$exco->post}}</span>
+              </div>
+            </div>
+          </div>
+    @endforeach
+      
 
-      <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-        <div class="member" data-aos="fade-up" data-aos-delay="100">
-          <div class="member-img">
-            <img src="{{asset('landingpage/assets/img/event1.png')}}" class="img-fluid" alt="">
-            <!-- <div class="social">
-              <a href=""><i class="bi bi-twitter"></i></a>
-              <a href=""><i class="bi bi-facebook"></i></a>
-              <a href=""><i class="bi bi-instagram"></i></a>
-              <a href=""><i class="bi bi-linkedin"></i></a>
-            </div> -->
-          </div>
-          <div class="member-info">
-            <h4>President Name</h4>
-            <span>President</span>
-          </div>
-        </div>
-      </div>
+     
 
-      <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-        <div class="member" data-aos="fade-up" data-aos-delay="200">
-          <div class="member-img">
-            <img src="{{asset('landingpage/assets/img/event2.png')}}" class="img-fluid" alt="">
-            <!-- <div class="social">
-              <a href=""><i class="bi bi-twitter"></i></a>
-              <a href=""><i class="bi bi-facebook"></i></a>
-              <a href=""><i class="bi bi-instagram"></i></a>
-              <a href=""><i class="bi bi-linkedin"></i></a>
-            </div> -->
-          </div>
-          <div class="member-info">
-            <h4>Vice President Name</h4>
-            <span>Vice President</span>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-        <div class="member" data-aos="fade-up" data-aos-delay="300">
-          <div class="member-img">
-            <img src="{{asset('landingpage/assets/img/event3.png')}}" class="img-fluid" alt="">
-            <!-- <div class="social">
-              <a href=""><i class="bi bi-twitter"></i></a>
-              <a href=""><i class="bi bi-facebook"></i></a>
-              <a href=""><i class="bi bi-instagram"></i></a>
-              <a href=""><i class="bi bi-linkedin"></i></a>
-            </div> -->
-          </div>
-          <div class="member-info">
-            <h4>Person Name</h4>
-            <span>Position name</span>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
-        <div class="member" data-aos="fade-up" data-aos-delay="400">
-          <div class="member-img">
-            <img src="{{asset('landingpage/assets/img/event4.png')}}" class="img-fluid" alt="">
-            <!-- <div class="social">
-              <a href=""><i class="bi bi-twitter"></i></a>
-              <a href=""><i class="bi bi-facebook"></i></a>
-              <a href=""><i class="bi bi-instagram"></i></a>
-              <a href=""><i class="bi bi-linkedin"></i></a>
-            </div> -->
-          </div>
-          <div class="member-info">
-            <h4>Person name</h4>
-            <span>Position Name</span>
-          </div>
-        </div>
-      </div>
+     
 
     </div>
 
